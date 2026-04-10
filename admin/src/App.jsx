5baf64479f43165755f3f2bd3fc2d223fdf21b40
rzryhn <rizarayhanw@gmail.com>
2026-04-10 14:40:29 +0700

    finalized

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
         <Routes>
           <Route path="/add" element={<Add url={url} />} />
           <Route path="/list" element={<List url={url} />} />
-          <Route path="/orders" element={<Orders />} />
+          <Route path="/orders" element={<Orders url={url} />} />
         </Routes>
       </div>
     </div>
